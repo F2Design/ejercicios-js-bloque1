7406f5daa134006eb4d38084c459bc9ae33047c0
Fredy <fredyfrasser@hotmail.com>
2024-11-18 18:15:35 -0500

    10 Ejercicios basicos de JavaScript del curso de Master en JavaScript 2025 de Victor Robles Web

--- 01-primer-programa/main.js
+++
@@ -1,7 +0,0 @@
-// alert("Hola, soy un mensaje");
-// alert("Hola, bienvenido al curso");
-
-console.log("Soy un msj desde JS");
-console.log("Hola, bienvenido al curso");
-
-console.log( 110 + 11 );
